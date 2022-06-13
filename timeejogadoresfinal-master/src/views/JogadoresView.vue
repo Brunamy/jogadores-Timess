5e4b7f4d0e1b46e13769c77492cd8bbfd451094a
Brunamy <87535917+Brunamy@users.noreply.github.com>
2022-06-13 15:35:32 -0300

    Add files via upload

---
+++ timeejogadoresfinal-master/src/views/JogadoresView.vue
@@ -0,0 +1,72 @@
+<script>
+import { v4 as uuid } from "uuid";
+export default {
+  data() {
+    return {
+      novo_times: "",
+      jogadores: [
+        { id: 1, name: "jogador 1", timeid: "1" },
+        { id: 2, name: "jogador 2", timeid: "2" },
+        { id: 3, name: "jogador 3", timeid: "3" },
+        { id: 4, name: "jogador 4", timeid: "4" },
+      ],
+    };
+  },
+  methods: {
+    salvar() {
+      if (this.novo_jogador !=="") {
+      const novo_id = uuid();
+      this.novo_jogadores.push({
+        id: novo_id,
+        name: this.novo_jogador,
+        name: this.novo_time,
+      });
+      this.novo_jogador = "";
+      this.novo_time = "";
+    }
+  },
+  excluir(jogador) {
+    const indice = this.novo_jogadores.indexOf(jogador);
+    this.novo_jogadores.splice(indice, 1);
+  },
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <div class="title">
+      <h2>Gerenciamento de Jogadores</h2>
+    </div>
+    <div class="form-input">
+      <input type="text" placeholder="Nome" />
+      <input type="text" placeholder="Time" />
+      <button @click="salvar">Salvar</button>
+    </div>
+    <div class="list-itens">
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Times</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="jogador in jogadores" :key="jogador.id">
+            <td>{{ jogador.id }}</td>
+            <td>{{ jogador.name }}</td>
+            <td>{{ jogador.timeid }}</td>
+            <td>
+              <button>Editar</button>
+              <button @click="excluir(jogador)">Excluir</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ timeejogadoresfinal-master/src/views/TimesView.vue
@@ -0,0 +1,67 @@
+<script>
+import { v4 as uuid } from "uuid";
+export default {
+  data() {
+    return {
+      novo_time: "",
+      times: [
+        { id: "c09cc3cc-e7fa-11ec-8fea-0242ac120002", name: "Time 1" },
+        { id: "b742b4f6-99b0-4eda-b2ca-c354b5301c53", name: "Time 2" },
+        { id: "db3b45dc-e7fa-11ec-8fea-0242ac120002", name: "Time 3" },
+        { id: "03ff2fce-e7fb-11ec-8fea-0242ac120002", name: "Time 4" },
+      ],
+    };
+  },
+  methods: {
+    salvar() {
+    if (this.novo_time !=="") {
+      const novo_id = uuid();
+      this.times.push( {
+        id: novo_id,
+        name: this.novo_time,
+      });
+      this.novo_time="";
+      
+    }
+    },
+  excluir(time) {
+    const indice = this.times.indexOf(time);
+    this.times.splice(indice, 1);
+  },
+  },
+};
+</script>
+<template>
+  <div class="container">
+    <div class="title">
+      <h2>Gerenciamento de Times</h2>
+    </div>
+    <div class="form-input">
+      <input type="text" required v-model="novo_time" @keyup.enter="salvar" />
+      <button @click="salvar">Salvar</button>
+    </div>
+    <div class="list-itens">
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="time in times" :key="time.id">
+            <td>{{ time.id }}</td>
+            <td>{{ time.name }}</td>
+            <td>
+              <button>Editar</button>
+              <button @click="excluir(time)">Excluir</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<style></style>
